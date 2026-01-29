c16d799ddc16272b22a2b31c232150277ed21fb6
Helder Russa <helder_russa@hotmail.com>
2026-01-29 15:10:51 +0000

    ch05: refactor: update live_orders_stabilized SQL model for improved filtering and add schema description and overrided materialization to view

--- chapter_05/transformations/dbt/ch05_dbt_transformations/models/gold/live_orders_stabilized.sql
+++ chapter_05/transformations/dbt/ch05_dbt_transformations/models/gold/live_orders_stabilized.sql
@@ -1,8 +1,14 @@
-WITH ranked AS (
-  SELECT 
-    *, 
-    ROW_NUMBER() OVER (PARTITION BY id ORDER BY ingest_ts DESC) AS row_num
+WITH filtered AS (
+  SELECT *
   FROM {{ source('bronze', 'live_orders') }}
+  WHERE DATE(created_at) = CURRENT_DATE()
+    AND ingest_ts >= TIMESTAMP_SUB(TIMESTAMP(CURRENT_DATE()), INTERVAL 48 HOUR)
+),
+ranked AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER (PARTITION BY id ORDER BY ingest_ts DESC) AS row_num
+  FROM filtered
 )
 SELECT
   CAST(id AS INT64) AS order_id,
@@ -14,4 +20,4 @@ SELECT
   CAST(created_at AS TIMESTAMP) AS created_at,
   CAST(ingest_ts AS TIMESTAMP) AS ingest_time
 FROM ranked
-WHERE row_num = 1 AND DATE(created_at) >= DATE(CURRENT_DATE())
+WHERE row_num = 1
